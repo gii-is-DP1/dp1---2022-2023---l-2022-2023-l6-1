4ec800e960ead3cd8c161434de7d77b2d6a5285a
mannavsic <mannavsic@alum.us.es>
2022-12-11 20:50:41 +0100

    Tests de Board

---
+++ src/test/java/org/springframework/samples/petclinic/board/BoardServiceTest.java
@@ -0,0 +1,28 @@
+package org.springframework.samples.petclinic.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class BoardServiceTest {
+	  @Autowired
+		protected BoardService boardService;
+
+		@Test
+		void shouldFindBoardById() {
+			Optional<Board> board = this.boardService.findById(1);
+			assertThat(board.get()).isEqualTo(1);
+
+		}
+
+		
+		
+		
+}
