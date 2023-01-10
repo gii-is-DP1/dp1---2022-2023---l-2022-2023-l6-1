4307af501483dea8a1cce18b10cce2805c404e85
mannavsic <mannavsic@alum.us.es>
2023-01-10 22:54:55 +0100

    Cambio de jugabilidad

--- src/main/java/org/springframework/samples/solitaire/board/BoardController.java
+++ src/main/java/org/springframework/samples/solitaire/board/BoardController.java
@@ -29,7 +29,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BoardController {
 
-	
+
 	private static final String VIEWS_BOARD = "board/chooseDifficult";
 	private static final String BOARD_1 = "board/board1";
 	private static final String BOARD_2 = "board/board2";
@@ -48,10 +48,15 @@ public class BoardController {
 	@GetMapping(value = "/startGame")
 	public String initCreationForm(Map<String, Object> model) { 
 		Board board = new Board();
-		
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		Player player = playerService.findByUsername(username);
+		Statistics statistics = statisticsService.findById(player.getId());
+
 		model.put("board", board);
 		model.put("playzone", startGame()); 
-		
+		statistics.setGames(statistics.getGames()+1);
+		statisticsService.saveStatistics(statistics);
 		return VIEWS_BOARD;
 	}
 	Integer i = 1;
@@ -65,24 +70,19 @@ public class BoardController {
 		List<Card> monton2 = cardService.findAllCardsDeck(6, 0);
 		List<Card> monton3 = cardService.findAllCardsDeck(7, 0);
 		List<Card> monton4 = cardService.findAllCardsDeck(8, 0);
-		
+
 		Integer TotalSize = monton1.size() + monton2.size() + monton3.size() + monton4.size();
+
+
+
 		if(TotalSize == 52) {
-			String username = SecurityContextHolder.getContext().getAuthentication().getName();
-			Player player = playerService.findByUsername(username);
-			Statistics statistics = statisticsService.findById(player.getId());
-			statistics.setGames(statistics.getGames()+1);
-			statistics.setGamesWon(statistics.getGamesWon()+1);
-			statistics.setTotalScore((statistics.getGamesWon()*10-statistics.getGamesLost()*6)/statistics.getGames());
-			statisticsService.saveStatistics(statistics);
 			return "board/youWin";
 		}
-		
 		model.put("now", new Date());
 		model.put("board", boardService.findById(1)); 
 		return BOARD_1;
 	}
- 
+
 	@GetMapping(value = "/difficult2")
 	public String creationMediumMode(Map<String, Object> model) {
 		Board board = new Board();
@@ -96,24 +96,30 @@ public class BoardController {
 		model.put("board", board);
 		return BOARD_3;
 	}
-	
+
 	@GetMapping(value = "/board/giveup")
 	public String creationGiveUp(Map<String, Object> model) {
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		Player player = playerService.findByUsername(username);
 		Statistics statistics = statisticsService.findById(player.getId());
-		statistics.setGames(statistics.getGames()+1);
-		statistics.setGamesLost(statistics.getGamesLost()+1);
+		statistics.setGamesWon(statistics.getGamesLost()+1);
 		statistics.setTotalScore((statistics.getGamesWon()*10-statistics.getGamesLost()*6)/statistics.getGames());
 		statisticsService.saveStatistics(statistics);
 		return "/board/giveUp";  
 	}
-	
+
 	@GetMapping(value = "/board/youwin")
 	public String creationYouWin(Map<String, Object> model) {
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		Player player = playerService.findByUsername(username);
+		Statistics statistics = statisticsService.findById(player.getId());
+		statistics.setGamesWon(statistics.getGamesWon()+1);
+		statistics.setTotalScore((statistics.getGamesWon()*10-statistics.getGamesLost()*6)/statistics.getGames());
+		statisticsService.saveStatistics(statistics);
+
 		return "/board/youWin";
 	}
-	
+
 
 	//	====================================Play=================================================================
 
@@ -136,10 +142,8 @@ public class BoardController {
 		List<Card> cartasBoard = this.cardService.findAllCardsBoardId(1);
 		List<Card> cartasF = this.cardService.findAllCardsBoardIdfalse(1);
 		List<Card> cartasPosicion = this.cardService.findCardByPosition(card.getXPosition());
-//		List<Card> cartasPosicionY = this.cardService.findCardByPositionY(0);
 		Integer num = card.getNumber();
-		String color = card.getColor();
-		
+
 		if(card.getNumber() == 1) {
 			switch(card.getSuit()) {
 			case "DIAMONDS" :
@@ -191,35 +195,34 @@ public class BoardController {
 				this.cardService.saveCard(card);
 				break;
 			}
-
 		}else if(card.getNumber() == 13){ 
 			for(Card c : cartasF) {
 				if(c.getNumber() == 12 && c.getYPosition()==0 && (c.getXPosition()==5 || c.getXPosition()==6 || c.getXPosition()==7 || c.getXPosition( )==8)) {
-					
+
 					switch(card.getSuit()) {
 					case "DIAMONDS" :
-						
+
 						card.setXPosition(5);
 						card.setYPosition(0);
 						card.setIsShowed(false);
 						this.cardService.saveCard(card);
 						break;
 					case "SPADES" :
-						
+
 						card.setXPosition(6);
 						card.setYPosition(0);
 						card.setIsShowed(false);
 						this.cardService.saveCard(card);
 						break;
 					case "HEARTS" :
-						
+
 						card.setXPosition(7);
 						card.setYPosition(0);
 						card.setIsShowed(false);
 						this.cardService.saveCard(card);
 						break;
 					default :
-						
+
 						card.setXPosition(8);
 						card.setYPosition(0);
 						card.setIsShowed(false);
@@ -261,190 +264,217 @@ public class BoardController {
 				}
 			}
 		}else{ 
-			List<Card> listaAuxiliar = new ArrayList<>();
-			for(Card c : cartasBoard) {
-				if(card.getYPosition()!=0) {
-					if(card.getXPosition() == c.getXPosition() && card.getYPosition() < c.getYPosition() 
-							&& card.getNumber() > c.getNumber() && c.getIsShowed()==true) {
-						listaAuxiliar.add(c);
-					}
-				}
-			}
-			for(Card c : cartasBoard) {
-				Card k = this.cardService.findAllCardsVacia(c.getXPosition(),c.getYPosition()+1);
-				if(c.getNumber() == num+1 && c.getColor() != color && k==null && c.getXPosition()!=1) {
-					//Poner comprobacion si hay una carta en esta pos ya para que no se amontonen 
-					for(Card c1 : cartasPosicion) {
-						if(c1.getYPosition() == card.getYPosition()-1 && c1.getIsShowed()== false) {
-							c1.setIsShowed(true);
-							this.cardService.saveCard(c1);
-						}
-					}
-					card.setXPosition(c.getXPosition());
-					card.setYPosition(c.getYPosition() + 1); 
-					this.cardService.saveCard(card);
-					Integer i = 1;
-					for(Card cartaAux : listaAuxiliar) {
-						cartaAux.setXPosition(c.getXPosition());
-						cartaAux.setYPosition(card.getYPosition()+i);
-						this.cardService.saveCard(cartaAux);
-						i++;
-					}
-				} 
-			}
-
 			for(Card c : cartasF) {
 				Card k = this.cardService.findAllCardsVacia(card.getXPosition(),card.getYPosition()+1);
 				if((card.getYPosition()==0 && card.getXPosition()==1) || k==null) {
 					if(c.getNumber() == num -1 && c.getSuit().equals(card.getSuit()) && c.getYPosition()==0 && (c.getXPosition()==5||
-							c.getXPosition()==6||c.getXPosition()==7||c.getXPosition( )==8)) {
+							c.getXPosition()==6||c.getXPosition()==7||c.getXPosition()==8)) {
 						for(Card c1 : cartasPosicion) {
 							if(c1.getYPosition() == card.getYPosition()-1 && c1.getIsShowed()== false) {
 								c1.setIsShowed(true);
 								this.cardService.saveCard(c1);
 							}
 						}
-						card.setXPosition(c.getXPosition());
+						card.setXPosition(c.getXPosition()); 
 						card.setYPosition(c.getYPosition()); 
 						card.setIsShowed(false);
 						this.cardService.saveCard(card);
 					}
 				}
 			}
-			model.put("board", boardService.findById(boardId));
-			
-			}
-
-
-			return new ModelAndView("redirect:/difficult1");
+			model.put("board", boardService.findById(boardId));			
 		}
+		return new ModelAndView("redirect:/difficult1");
+	}
 
 
 
+	@GetMapping(value = "/board/moveCardP/{card1Id}&{cardId}")
+	public ModelAndView moveCard(@PathVariable("card1Id") int card1Id, @PathVariable("cardId") int cardId, Map<String, Object> model ) {
+		Card card = this.cardService.findCardById(cardId); //--> donde
+		Card card1 = this.cardService.findCardById(card1Id); //--> mazo
+		Integer boardId = card1.getBoard().getId();
+		//Cambiar logica para el resto de modos
+		List<Card> cartasBoard = this.cardService.findAllCardsBoardId(1);
+		List<Card> cartasF = this.cardService.findAllCardsBoardIdfalse(1);
+		List<Card> cartasPosicion = this.cardService.findCardByPosition(card1.getXPosition());
+		Integer num = card1.getNumber();
 
-		@GetMapping(value = "/board/moveCardDeck")
-		public ModelAndView moveCardDeck(Map<String, Object> model ) {
-//			Card card = this.cardService.findCardById(cardId);
-//			Integer boardId = card.getBoard().getId();
-			List<Card> BarajaInicial = this.cardService.findAllCardsDeck(0, 0);
-			List<Card> Carta10 = this.cardService.findAllCardsDeck(1, 0);
-			if(BarajaInicial.size() > 0) {
-				int num = (int) (Math.random() * (BarajaInicial.size())) ;
-				Card initCard = BarajaInicial.get(num);
-				if(Carta10.isEmpty()) {
-					initCard.setXPosition(1);
-					initCard.setYPosition(0);
-					initCard.setIsShowed(true);
-					this.cardService.saveCard(initCard);
-					Carta10.add(initCard);
-					BarajaInicial.remove(initCard);
-				}else {
-					Card c = Carta10.get(0);
-					c.setXPosition(0);
-					c.setYPosition(0);
-					c.setIsShowed(false);
-					this.cardService.saveCard(c);
-					initCard.setXPosition(1);
-					initCard.setYPosition(0);
-					initCard.setIsShowed(true);
-					this.cardService.saveCard(initCard);
-					BarajaInicial.add(c);
-					Carta10.remove(c);
+		List<Card> listaAuxiliar = new ArrayList<>();
+		for(Card c : cartasBoard) {
+			if(card1.getYPosition()!=0) {
+				if(card1.getXPosition() == c.getXPosition() && card1.getYPosition() < c.getYPosition() 
+						&& card1.getNumber() > c.getNumber() && c.getIsShowed()==true) {
+					listaAuxiliar.add(c);
 				}
 			}
-				
-//			}else if(BarajaInicial.size() == 0) {
-//					lsAuxiliar = this.cardService.findAllCardsDeck(1, 0);
-//					for(int i = 0; i< lsAuxiliar.size();i++) {
-//						Card c = lsAuxiliar.get(i);
-//						c.setXPosition(0);
-//						c.setYPosition(0);
-//						c.setIsShowed(false);
-//						this.cardService.saveCard(c);
-//						
-//					}
-//			}
-			
-			model.put("board", boardService.findById(1));
-			return new ModelAndView("redirect:/difficult1");
 		}
-
-
-		//	====================================PlayZone=================================================================
-
-		public PlayZone startGame() {
-			List<Card> cards = cardService.findAll();
-			Card cardAux = new Card();
-			PlayZone playzone = new PlayZone();
-			List<Card> listaAuxResultado = new ArrayList<>();
-			for(Card c1:cards) {
-				c1.setXPosition(0);
-				c1.setYPosition(0);
-				c1.setIsShowed(false);
-				this.cardService.saveCard(c1); 
+		Card k = this.cardService.findAllCardsVacia(card.getXPosition(),card.getYPosition()+1);
+		if(k == null) {
+			for(Card c1 : cartasPosicion) {
+				if(c1.getYPosition() == card1.getYPosition()-1 && c1.getIsShowed()== false) {
+					c1.setIsShowed(true);
+					this.cardService.saveCard(c1);
+				}
 			}
-			for(int c = 2; c<9;c++) {
-				Integer nF = 2;
-				for(int f = 2; f<=9;f++) {
-					if(nF == c ) {
-						Random numRandom = new Random();
-						cardAux = cards.get(numRandom.nextInt(cards.size()+1));
-						cards.remove(cardAux);
-						cardAux.setIsShowed(true);
-						cardAux.setXPosition(c);
-						cardAux.setYPosition(f);
-						cardService.saveCard(cardAux);
-						listaAuxResultado.add(cardAux);
 
-						break;
-					}else {
-						Random numRandom = new Random();
-						cardAux = cards.get(numRandom.nextInt(cards.size()+1));
-						cards.remove(cardAux);
-						cardAux.setIsShowed(false);
-						cardAux.setXPosition(c);
-						cardAux.setYPosition(f);
-						cardService.saveCard(cardAux);
-						listaAuxResultado.add(cardAux);
-						nF++;
+			card1.setXPosition(card.getXPosition());
+			card1.setYPosition(card.getYPosition() + 1); 
+			this.cardService.saveCard(card1);
+			Integer i = 1;
+			for(Card cartaAux : listaAuxiliar) {
+				cartaAux.setXPosition(card.getXPosition());
+				cartaAux.setYPosition(card1.getYPosition()+i);
+				this.cardService.saveCard(cartaAux);
+				i++;
+			}
+		}
+		for(Card c : cartasF) {
+			Card k1 = this.cardService.findAllCardsVacia(card1.getXPosition(),card1.getYPosition()+1);
+			if((card1.getYPosition()==0 && card1.getXPosition()==1) || k1==null) {
+				if(c.getNumber() == num -1 && c.getSuit().equals(card1.getSuit()) && c.getYPosition()==0 && (c.getXPosition()==5 ||
+						c.getXPosition()==6 || c.getXPosition()==7 || c.getXPosition( )==8)) {
+					for(Card c1 : cartasPosicion) {
+						if(c1.getYPosition() == card1.getYPosition()-1 && c1.getIsShowed()== false) {
+							c1.setIsShowed(true);
+							this.cardService.saveCard(c1);
+						}
 					}
+					card1.setXPosition(c.getXPosition());
+					card1.setYPosition(c.getYPosition()); 
+					card1.setIsShowed(false);
+					this.cardService.saveCard(card1);
 				}
 			}
-			playzone.setPlayZoneCard(listaAuxResultado);
-			return playzone;
 		}
+		model.put("board", boardService.findById(boardId));
+		return new ModelAndView("redirect:/difficult1");
+	}
 
 
-
-		//	====================================Card=================================================================
-
-
-		@GetMapping(value = "/card/{cardId}/edit")
-		public String initUpdateCardForm(@PathVariable("cardId") int cardId, Model model) {
-			Card card = this.cardService.findCardById(cardId);
-			if(card.getNumber() == 1) {
-				return "redirect: /board/moveCard/{cardId}";
+	@GetMapping(value = "/board/moveCardDeck")
+	public ModelAndView moveCardDeck(Map<String, Object> model ) {
+		//			Card card = this.cardService.findCardById(cardId);
+		//			Integer boardId = card.getBoard().getId();
+		List<Card> BarajaInicial = this.cardService.findAllCardsDeck(0, 0);
+		List<Card> Carta10 = this.cardService.findAllCardsDeck(1, 0);
+		if(BarajaInicial.size() > 0) {
+			int num = (int) (Math.random() * (BarajaInicial.size())) ;
+			Card initCard = BarajaInicial.get(num);
+			if(Carta10.isEmpty()) {
+				initCard.setXPosition(1);
+				initCard.setYPosition(0);
+				initCard.setIsShowed(true);
+				this.cardService.saveCard(initCard);
+				Carta10.add(initCard);
+				BarajaInicial.remove(initCard);
 			}else {
-				model.addAttribute(card);
-				return "card/createOrUpdateCardForm";
+				Card c = Carta10.get(0);
+				c.setXPosition(0);
+				c.setYPosition(0);
+				c.setIsShowed(false);
+				this.cardService.saveCard(c);
+				initCard.setXPosition(1);
+				initCard.setYPosition(0);
+				initCard.setIsShowed(true);
+				this.cardService.saveCard(initCard);
+				BarajaInicial.add(c);
+				Carta10.remove(c);
 			}
-
 		}
 
-		@PostMapping(value = "/card/{cardId}/edit")
-		public String processUpdateCardForm(@Valid Card card, BindingResult result,
-				@PathVariable("cardId") int cardId) {
-			if (result.hasErrors()) {
-				return "card/createOrUpdateCardForm";
-			}
-			else {
-				card.setId(cardId);
-				this.cardService.saveCard(card);
-				return "redirect:/difficult1";
-			}
+		//			}else if(BarajaInicial.size() == 0) {
+		//					lsAuxiliar = this.cardService.findAllCardsDeck(1, 0);
+		//					for(int i = 0; i< lsAuxiliar.size();i++) {
+		//						Card c = lsAuxiliar.get(i);
+		//						c.setXPosition(0);
+		//						c.setYPosition(0);
+		//						c.setIsShowed(false);
+		//						this.cardService.saveCard(c);
+		//						
+		//					}
+		//			}
 
+		model.put("board", boardService.findById(1));
+		return new ModelAndView("redirect:/difficult1");
+	}
+
+
+	//	====================================PlayZone=================================================================
+
+	public PlayZone startGame() {
+		List<Card> cards = cardService.findAll();
+		Card cardAux = new Card();
+		PlayZone playzone = new PlayZone(); 
+		List<Card> listaAuxResultado = new ArrayList<>();
+		for(Card c1:cards) {
+			c1.setXPosition(0);
+			c1.setYPosition(0);
+			c1.setIsShowed(false); 
+			this.cardService.saveCard(c1); 
+		}
+		for(int c = 2; c<9;c++) {
+			Integer nF = 2;
+			for(int f = 2; f<=9;f++) {
+				if(nF == c ) {
+					Random numRandom = new Random();
+					cardAux = cards.get(numRandom.nextInt(cards.size()+1));
+					cards.remove(cardAux);
+					cardAux.setIsShowed(true);
+					cardAux.setXPosition(c);
+					cardAux.setYPosition(f);
+					cardService.saveCard(cardAux);
+					listaAuxResultado.add(cardAux);
+
+					break;
+				}else {
+					Random numRandom = new Random();
+					cardAux = cards.get(numRandom.nextInt(cards.size()+1));
+					cards.remove(cardAux);
+					cardAux.setIsShowed(false);
+					cardAux.setXPosition(c);
+					cardAux.setYPosition(f);
+					cardService.saveCard(cardAux);
+					listaAuxResultado.add(cardAux);
+					nF++;
+				}
+			}
+		}
+		playzone.setPlayZoneCard(listaAuxResultado);
+		return playzone;
+	}
+
+
+
+	//	====================================Card=================================================================
+
+
+	@GetMapping(value = "/card/{cardId}/edit")
+	public String initUpdateCardForm(@PathVariable("cardId") int cardId, Model model) {
+		Card card = this.cardService.findCardById(cardId);
+		if(card.getNumber() == 1) {
+			return "redirect: /board/moveCard/{cardId}";
+		}else {
+			model.addAttribute(card);
+			return "card/createOrUpdateCardForm";
 		}
 
 	}
+
+	@PostMapping(value = "/card/{cardId}/edit")
+	public String processUpdateCardForm(@Valid Card card, BindingResult result,
+			@PathVariable("cardId") int cardId) {
+		if (result.hasErrors()) {
+			return "card/createOrUpdateCardForm";
+		}
+		else {
+			card.setId(cardId);
+			this.cardService.saveCard(card);
+			return "redirect:/difficult1";
+		}
+
+	}
+
+}
 
 
--- src/main/webapp/WEB-INF/jsp/board/board1.jsp
+++ src/main/webapp/WEB-INF/jsp/board/board1.jsp
@@ -25,20 +25,47 @@
 					<tr>
 						<td>
 							<petclinic:menuItem active="${name eq 'play'}" url="/"
-								title="Possible Cards To Move" dropdown="${true}">
-								<ul class="dropdown-menu">
-								<c:forEach items="${board.cards}" var="card">
-									<c:if test="${card.isShowed == true }">
-										<li>
-											<a href="<c:url value="/board/moveCard/${card.id}"/>" >${card.number}-${card.suit}</a>
-										</li>
-									
-									</c:if>
-									</c:forEach>
-								</div>
-								
-								</ul>
-							</petclinic:menuItem>
+                                title="Possible Cards To Move" dropdown="${true}">
+                                <ul class="dropdown-menu">
+                                <c:forEach items="${board.cards}" var="card">
+                                    <c:forEach items="${board.cards}" var="card1">
+                                        <c:if test="${card.isShowed == true && card1.isShowed == true && card.number > 1}">
+                                            <c:choose>                                            	
+                                                <c:when test="${card1.number == card.number-1 && card1.color != card.color && card.getYPosition()>1 &&
+                                                 card1.getXPosition()!=card.getXPosition() && card1.getYPosition()!=card.getYPosition()+1}">
+                                                    <li>
+                                                        ${card1.number}-${card1.suit}
+                                                        <li>
+                                                        	<h5>Goes to</h5>
+                                                            <a href="<c:url value="/board/moveCardP/${card1.id}&${card.id}"/>" >${card.number}-${card.suit}</a>
+                                                        </li>
+                                                    </li>
+                                                </c:when>                                              
+                                            </c:choose>
+                                        </c:if>                                   
+                                    	<c:if test="${card1.number == card.number+1 && card1.suit == card.suit && card.isShowed == false && card1.isShowed == true && card.getYPosition() == 0 && (card.getXPosition() == 5 || card.getXPosition() == 6 || card.getXPosition() == 7 || card.getXPosition() == 8)}">                                  		
+                                    		<li>
+                                    			<h5>To the pile</h5>
+                                    	 	  	<a href="<c:url value="/board/moveCard/${card1.id}"/>" >${card1.number}-${card1.suit}</a>
+                                    		</li>                                  		
+                                  		</c:if>
+                                    </c:forEach>
+                                    
+                                    <c:if test="${card.isShowed == true && card.number == 1}">
+                                        <li>
+                                        	<h5>To the pile</h5>
+                                            <a href="<c:url value="/board/moveCard/${card.id}"/>" >${card.number}-${card.suit}</a>
+                                        </li>
+                                    </c:if>
+                                    <c:if test="${card.isShowed == true && card.number == 13 && card.getYPosition()!=2}">
+                                        <li>
+                                        	<h5>To the top</h5>
+                                            <a href="<c:url value="/board/moveCard/${card.id}"/>" >${card.number}-${card.suit}</a>
+                                        </li>
+                                    </c:if>
+                                </c:forEach>
+                                </ul>
+                            </petclinic:menuItem>
 					
 						<br>
 						<center>
