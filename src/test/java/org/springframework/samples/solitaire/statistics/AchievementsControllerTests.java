47507d0604589f0bf88f67a200a2b91717bdb3fc
manortbla <manortbla@alum.us.es>
2023-01-10 21:33:52 +0100

    AchievementsControllerTests completado

--- src/test/java/org/springframework/samples/solitaire/statistics/AchievementsControllerTests.java
+++ src/test/java/org/springframework/samples/solitaire/statistics/AchievementsControllerTests.java
@@ -3,8 +3,6 @@ package org.springframework.samples.solitaire.statistics;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,14 +16,13 @@ import org.springframework.samples.solitaire.configuration.SecurityConfiguration
 import org.springframework.samples.solitaire.friendRequest.FriendRequestService;
 import org.springframework.samples.solitaire.friends.FriendsService;
 import org.springframework.samples.solitaire.player.Player;
-import org.springframework.samples.solitaire.player.PlayerController;
 import org.springframework.samples.solitaire.player.PlayerService;
-import org.springframework.samples.solitaire.statistics.AchievementsController;
 import org.springframework.samples.solitaire.user.AuthoritiesService;
 import org.springframework.samples.solitaire.user.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 @WebMvcTest(controllers = AchievementsController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class AchievementsControllerTests {
@@ -98,10 +95,16 @@ public class AchievementsControllerTests {
 	@Test
 	void testInitCreationForm() throws Exception {
 		mockMvc.perform(get("/achievements")).andExpect(status().isOk())
-		.andExpect(model().attributeExists("condition_unlocked"));
+		.andExpect(view().name("achievements"));
 	}
 	
-	
+	@WithMockUser(value = "spring")
+	@Test
+	void testInitUnlockedForm() throws Exception {
+		mockMvc.perform(get("/achievements/achievementsStatistics")).andExpect(status().isOk())
+		.andExpect(view().name("achievements/achievementsStatistics"))
+		.andReturn();
+	}
 	
 	
 	
